f7d51b4973cad8906b471ab005884474ee42f5fb
Ginger Williams <gingerwilliams@MacBook-Pro.attlocal.net>
2019-07-03 14:09:49 -0400

    add new test | add intellisense for Jest

--- client/src/__tests__/Test.test.js
+++ client/src/__tests__/Test.test.js
@@ -1,6 +1,26 @@
-import { add } from '../components/Test';
+import { add, total } from '../components/Test';
 
+//Mock Function
+//const mock = jest.fn(callback function);
+
+//unit test
+//It only tests one thing
 test('add', () => {
-    const value = add(2, 3)
-    expect(value).toBe(5);
+    // const value = add(2, 3)
+    // expect(value).toBe(5);
+    expect(add(2, 3)).toBe(5);
+    expect(add).toHaveBeenCalledTimes(1);
 });
+
+//integration test
+//test things working together
+test('total', () => {
+    expect(total(5, 20)).toBe('$25');
+});
+
+// Mock functions, test that function when we dont
+// have access to that live function like database 
+// calls
+
+
+
--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -1,1 +1,8 @@
+//Unit
 export const add = (x, y) => x + y;
+
+//Integration - relies on add to work
+export const total = (shipping, subTotal) => {
+    return `$${add(shipping, subTotal)}`;
+}
+
