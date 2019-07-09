d87cec430eaca3d449da2082c10cc3118d9c9fd7
Ginger Williams <gingerwilliams@MacBook-Pro.attlocal.net>
2019-07-09 15:14:12 -0400

    update js to jsx

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -86,15 +86,15 @@
 /************************************************************************/
 /******/ ({
 
-/***/ "./client/index.js":
-/*!*************************!*\
-  !*** ./client/index.js ***!
-  \*************************/
+/***/ "./client/index.jsx":
+/*!**************************!*\
+  !*** ./client/index.jsx ***!
+  \**************************/
 /*! no exports provided */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _src_components_App__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./src/components/App */ \"./client/src/components/App.jsx\");\n\n\n\nreact_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render(react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_src_components_App__WEBPACK_IMPORTED_MODULE_2__[\"default\"], null), document.querySelector('#root'));\n\n//# sourceURL=webpack:///./client/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _src_components_App__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./src/components/App */ \"./client/src/components/App.jsx\");\n\n\n\nreact_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render(react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_src_components_App__WEBPACK_IMPORTED_MODULE_2__[\"default\"], null), document.querySelector('#root'));\n\n//# sourceURL=webpack:///./client/index.jsx?");
 
 /***/ }),
 
@@ -615,13 +615,13 @@ eval("var logLevel = \"info\";\n\nfunction dummy() {}\n\nfunction shouldLog(leve
 /***/ }),
 
 /***/ 0:
-/*!*********************************************************************************!*\
-  !*** multi (webpack)-dev-server/client?http://localhost:8000 ./client/index.js ***!
-  \*********************************************************************************/
+/*!**********************************************************************************!*\
+  !*** multi (webpack)-dev-server/client?http://localhost:8000 ./client/index.jsx ***!
+  \**********************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("__webpack_require__(/*! /Users/gingerwilliams/source/node/node-mysql/node_modules/webpack-dev-server/client/index.js?http://localhost:8000 */\"./node_modules/webpack-dev-server/client/index.js?http://localhost:8000\");\nmodule.exports = __webpack_require__(/*! ./client/index.js */\"./client/index.js\");\n\n\n//# sourceURL=webpack:///multi_(webpack)-dev-server/client?");
+eval("__webpack_require__(/*! /Users/gingerwilliams/source/node/node-mysql/node_modules/webpack-dev-server/client/index.js?http://localhost:8000 */\"./node_modules/webpack-dev-server/client/index.js?http://localhost:8000\");\nmodule.exports = __webpack_require__(/*! ./client/index.jsx */\"./client/index.jsx\");\n\n\n//# sourceURL=webpack:///multi_(webpack)-dev-server/client?");
 
 /***/ })
 
