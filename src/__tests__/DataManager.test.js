578204c3869496c58f0e06dadb901486dbc7d377
Ginger Williams <gingerwilliams@MacBook-Pro-2.attlocal.net>
2019-07-18 16:12:35 -0400

    added test to datamanager

---
+++ src/__tests__/DataManager.test.js
@@ -0,0 +1,19 @@
+import DataManager from '../components/DataManager';
+
+//test crud operations
+it('DataManager gets tasks', () => {
+    //create a new instance of datamanager
+    const dataManager = new DataManager();
+    const tasks = dataManager.getTasks();
+    expect(tasks.length).toBe(0);
+})
+
+it('DataManager adds new tasks', () => {
+    const dataManager = new DataManager();
+    dataManager.addTasks('Get list of Rooms');
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    console.log(tasks);
+    // expect(tasks).toHaveProperty('length');
+    // expect(tasks.length).toBe(1);
+    // expect(tasks[0].name).toBe('Get list of Rooms');
+})
