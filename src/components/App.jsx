9797a8f46cd8ad87c387cb361607178fb6d8e308
Ginger Williams <gingerwilliams@MacBook-Pro-2.attlocal.net>
2019-07-18 13:53:42 -0400

    clean up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,6 @@ import React, { Fragment, Component } from 'react';
 import AddForm from './AddForm';
 
 class App extends Component {
-
-    // componentDidMount() {
-    //     fetch('https://api.myjson.com/bins/15psn9')
-    //         .then(result => result.json())
-    //         .then(rowData => this.setState({todo : rowData}))
-    // }
-
     render() {
         return (
             <Fragment>
